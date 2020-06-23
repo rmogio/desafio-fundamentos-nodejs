c144327f569b7ccef1813d195391d6bfad578dbe
Ricardo Mogio <ricardo.mogio@ymail.com>
2020-06-23 10:57:29 -0300

    Finalizado o dessafio

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import app from './app';
 
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+const door = 3300;
+
+app.listen(door, () => {
+  // eslint-disable-next-line no-console
+  console.log(`'🚀 Server started on port ${door}!'`);
 });
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
+    if (value < 0) {
+      throw Error('Value of transaction couldnot be negative');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You dont have enough founds');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
